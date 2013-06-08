db6197aa27db10ff41cded6415bfb136d0147d44
James Moore <james@restphone.com>
2013-06-08 00:30:49 -0600

    Added implicit methods for liftS and liftE

--- src/main/scala/training/EitherTExample.scala
+++ src/main/scala/training/EitherTExample.scala
@@ -27,6 +27,13 @@ object EitherTExample {
       def apply[A](st: QueryStateS[String \/ A]): QueryStateES[A] = EitherT(st)
       def liftE[A](ea: String \/ A): QueryStateES[A] = apply(Applicative[QueryStateS].point(ea))
       def liftS[A](st: QueryStateS[A]): QueryStateES[A] = MonadTrans[ET].liftM(st)
+
+      implicit class HasLiftE[A](ea: String \/ A) {
+        def liftE = QueryStateES.liftE[A](ea)
+      }
+      implicit class HasLiftS[A](qs: QueryStateS[A]) {
+        def liftS: QueryStateES[A] = QueryStateES.liftS(qs)
+      }
     }
 
     def runQuery(str: String, model: Model): QueryStateES[QueryResult] = for {
@@ -34,9 +41,20 @@ object EitherTExample {
       res <- performQuery(query, model)
     } yield res
 
+    // Pull in the implicit classes from QueryStateES
+    import QueryStateES._
+    
     def parseQuery(s: String): QueryStateES[StatsQuery] = for {
       state <- QueryStateES.liftS(State.get[QueryState])
       result <- QueryStateES.liftE("TODO".left[StatsQuery])
+      
+      // Or, if you prefer to use implicit methods:
+      state1 <- State.get[QueryState].liftS
+      result1 <- "TODO".left[StatsQuery].liftE
+      
+      // state1 and result1 are almost the same as state and result, they're just
+      // using implicit classes to allow using liftS and liftE as methods instead 
+      // of functions.
     } yield result
 
     def performQuery(q: StatsQuery, m: Model): QueryStateES[QueryResult] = QueryStateES.liftE("TODO".left)
